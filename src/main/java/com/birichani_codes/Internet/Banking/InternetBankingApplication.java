b0f3c46f2edbf8f702ce07d35e152acac8d2c6f8
birichani-codes <birichani.solomon@gmail.com>
2024-05-08 01:32:13 +0000

    setting the project structure and dependecies management

---
+++ src/main/java/com/birichani_codes/Internet/Banking/InternetBankingApplication.java
@@ -0,0 +1,13 @@
+package com.birichani_codes.Internet.Banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/birichani_codes/Internet/Banking/InternetBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birichani_codes.Internet.Banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
